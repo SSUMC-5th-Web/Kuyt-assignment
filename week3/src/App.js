74e2bc4df71199ce198e92bfee289618d5a8281e
dvp-tae <th3016@naver.com>
2023-10-16 17:01:01 +0900

    week3 이벤트 수정

--- week3/src/App.js
+++ week3/src/App.js
@@ -1,27 +1,21 @@
-import { useEffect, useState } from "react";
 import Movie from "./component/Movie";
-import MovieDetail from "./component/MovieDetail";
 import { movies } from "./movieDummy";
 
 function App() {
-  const [detail, setDetail] = useState(false);
-  const [contentIndex, setContentIndex] = useState(null);
-
-  // useEffect(()=>{
-  //   console.log(contentIndex);
-  // },[contentIndex])
 
   return (
     <div className="app">
-      <div className="movie">
-        {movies.results.map((item,index)=>([
-          <Movie item={item} index={index} setDetail={setDetail} setContentIndex={setContentIndex}/>
-        ]))}
-      </div>
-      <div className="movie-detail">
-        {movies.results.map((item,index)=>([
-          <MovieDetail item={item} index={index} detail={detail} contentIndex={contentIndex} />
-        ]))}
+      <div className="app-container">
+        {movies.results.map((item)=>{
+          return (
+            <Movie
+              poster_path={item.poster_path}
+              title={item.title}
+              vote_average={item.vote_average}
+              overview={item.overview}
+              />
+          );
+        })}
       </div>
     </div>
   )
--- week3/src/component/Movie.jsx
+++ week3/src/component/Movie.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { useState } from "react";
 
-const Movie = ({item, index, setDetail, setContentIndex}) => {
-    const showDetail =(index)=>{
-        setDetail(true);
-        // console.log(index);
-        setContentIndex(index);
-    }
-    const hideDetail =()=>{
-        setDetail(false);
+const Movie = ({poster_path, title, vote_average, overview}) => {
+  const [detail, setDetail] = useState(false);
 
-    }
+  const showDetail =()=>{
+      setDetail(true);
+  }
+  const hideDetail =()=>{
+      setDetail(false);
+  }
   return (
-    <div className='container'>
-      <div className='movie-card'>
-        <img className='movie-poster' src={item?.poster_path} onMouseOver={()=>showDetail(index)} onMouseLeave={hideDetail}/>
-        <div className='movie-info'>
-          <span>{item?.title}</span>
-          <span>{item?.vote_average}</span>
+    <div>
+      <div className='container' onMouseEnter={showDetail}>
+        <div className='movie-card'>
+          <img className='movie-poster' src={poster_path} alt='영화 포스터 대체'/>
+          <div className='movie-info'>
+            <span>{title}</span>
+            <span>{vote_average}</span>
+          </div>
         </div>
+        {detail&&(
+          <div className='detail-container' onMouseLeave={hideDetail}>
+            <p>{title}</p>
+            <span>{overview}</span>
+          </div>
+        )}
       </div>
     </div>
   )
